b49c34500556b27c85f8834f7f83537727e94472
Taihei Kojima <taikojim@yahoo-corp.jp>
2017-01-08 21:30:47 +0900

    インデント修正

--- TCP/tcp_client2.py
+++ TCP/tcp_client2.py
@@ -3,22 +3,23 @@ import sys
 import socket
 from contextlib import closing
 
-def main():
-  host = '127.0.0.1'
-  port = 4000
-  bufsize = 4096
 
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  with closing(sock):
-    sock.connect((host, port))
-    while True:
-      print("Input Message")
-      line = sys.stdin.readline().rstrip()
-      if len(line) == 0:
-        break
-      sock.send(line.encode('utf-8'))
-      print(sock.recv(bufsize))
-  return
+def main():
+    host = '127.0.0.1'
+    port = 4000
+    bufsize = 4096
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    with closing(sock):
+        sock.connect((host, port))
+        while True:
+            print("Input Message")
+            line = sys.stdin.readline().rstrip()
+            if len(line) == 0:
+                break
+            sock.send(line.encode('utf-8'))
+            print(sock.recv(bufsize))
+        return
 
 if __name__ == '__main__':
     main()
